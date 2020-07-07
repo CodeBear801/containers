fb8b585496b47d4439ee6695e354db5be77bf880
CoderBear801 <coderxiong@gmail.com>
2020-07-07 15:32:09 -0700

    fix: remove changes

--- docker/golang-dev/go.mod
+++
@@ -1,8 +0,0 @@
-module example.com/sample
-
-go 1.13
-
-require (
-	github.com/gorilla/mux v1.7.4
-	github.com/stretchr/testify v1.6.0
-)
--- docker/golang-dev/users/interfaces.go
+++
@@ -1,7 +0,0 @@
-package users
-
-// UserRepository abstracts accesses to users-related data.
-type UserRepository interface {
-	ExistsByUsername(string) (bool, error)
-	ExistsByEmail(string) (bool, error)
-}
